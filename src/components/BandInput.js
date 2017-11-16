cea0b68829cfae46b5365059b7060456181f6836
stopvoldemort <david.squires1@gmail.com>
2017-11-16 15:21:59 -0500

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 class BandInput extends Component {
+
+  state = {
+    text: ""
+  }
+
+  handleChange = (ev) => {
+    const newText = ev.target.value
+    this.setState({text: newText})
+  }
+
+  handleSubmit = (ev) => {
+    ev.preventDefault()
+    this.props.store.dispatch({type: "ADD_BAND", band: this.state.text})
+    this.setState({text: ""})
+  }
+
   render() {
     return (
       <div>
-        bands component
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="enter band name" onChange={this.handleChange} value={this.state.text} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 class Bands extends Component {
+
+
+  bandLis = () => (this.props.store.getState().bands.map((band,idx) => (<li key={idx}>{band}</li>)))
   render(){
+    console.log(this.props);
     return (
       <div>
-        band input
+        <ul>
+          {this.bandLis()}
+        </ul>
       </div>
     );
   }
